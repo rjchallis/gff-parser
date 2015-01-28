24a41696f26f36f68c64081082740f01ba23a7d6
Richard Challis <rjchallis@gmail.com>
2015-01-28 12:52:24 +0000

    Force Genomic hasParent

--- GFFTree/NCBI.pm
+++ GFFTree/NCBI.pm
@@ -38,6 +38,7 @@ sub new {
     $gff->add_expectation('gene','hasParent','region','force');
 	$gff->add_expectation('match','hasParent','region','force');
 	$gff->add_expectation('cDNA_match','hasParent','region','force');
+	$gff->add_expectation('Genomic','hasParent','region','force');
 	$gff->add_expectation('mrna','hasParent','gene','find');
 	$gff->add_expectation('trna','hasParent','gene','find');
 	$gff->add_expectation('transcript','hasParent','gene','find');
