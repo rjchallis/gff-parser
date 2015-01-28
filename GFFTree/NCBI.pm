6767e281e4e9f9f98a42298e136e875a291f9cc4
Richard Challis <rjchallis@gmail.com>
2015-01-28 12:22:23 +0000

    force match and cdna_match to have region as parent

--- GFFTree/NCBI.pm
+++ GFFTree/NCBI.pm
@@ -36,6 +36,8 @@ sub new {
     my $options = shift;
     my $gff = GFFTree->new($options);
     $gff->add_expectation('gene','hasParent','region','force');
+	$gff->add_expectation('match','hasParent','region','force');
+	$gff->add_expectation('cDNA_match','hasParent','region','force');
 	$gff->add_expectation('mrna','hasParent','gene','find');
 	$gff->add_expectation('trna','hasParent','gene','find');
 	$gff->add_expectation('transcript','hasParent','gene','find');
