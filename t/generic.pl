0cf30ab398e56af6c6d98f271b8889dff5cbee94
Richard Challis <rjchallis@gmail.com>
2015-03-26 10:47:26 +0000

    altered tests to allow for new lacks_id syntax

--- t/generic.pl
+++ t/generic.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -w
 
 use strict;
-use Test::More tests => 21;
+use Test::More tests => 20;
 use GFFTree;
 
 
@@ -11,16 +11,18 @@ ok( defined $gff, 'new()' );
 
 is($gff->add_expectation('exon','<[_start,_end]','PARENT','warn'), 1, 'add_expectation()');
 $gff->add_expectation('mrna','hasParent','gene','force');
+$gff->add_expectation('trna','hasParent','gene','force');
 is($gff->multiline('cds'),1,'multiline()');
 
 is($gff->is_multiline('cds'),1,'is_multiline()');
 $gff->multiline('cdna_match');
+$gff->multiline('trna');
 
 is($gff->name('GFF'), 'GFF', 'name()');
 
 is($gff->map_types({'initial' => 'exon', 'internal' => 'exon', 'terminal' => 'exon'}),3,'map_types()');
 
-$gff->lacks_id('make');
+$gff->lacks_id('all','make');
 is($gff->lacks_id(),'make','lacks_id()');
 
 $gff->undefined_parent('make');
@@ -28,19 +30,20 @@ is($gff->undefined_parent(),'make','undefined_parent()');
 
 $gff->separator('\t');
 
+my $comment_count = $gff->has_comments('#',['\[','\]']);
+#my $comment_count = $gff->has_comments();
+print "file has $comment_count comment types\n";
 
 ok($gff->parse_file(),'parse_file()');
 
 # Test 10
 ok($gff->validate_all('exon'),"validate_all('exon')");
 
+is($gff->order_features('gene'),19,"order_features('gene')");
+
+is(my @genes = $gff->by_type('gene'),19,"by_type('gene') as array");
+
 ok(my $gene = $gff->by_type('gene'),"by_type('gene') as scalar");
-
-is(my @genes = $gff->by_type('gene'),18,"by_type('gene') as array");
-
-is($gff->order_features('gene'),18,"order_features('gene')");
-
-is($gene->next_feature('exon')->name,'id5',"next_feature('exon')");
 
 ok($gene = $gff->by_id('gene1'),"by_id()");
 
@@ -54,14 +57,20 @@ is(length $gene->as_string(), 136 ,"\$gene->as_string()");
 my $cds = $gff->by_id('cds2');
 is($cds->{attributes}->{'_start_array'}[0],2227,"\$cds->{attributes}->{'_start_array'}");
 
-# Test 20
 is(length $cds->as_string(), 609 ,"\$cds->as_string()");
 
 $gene = $gff->by_id('gene9');
 $gene->fill_gaps('cds','5utr','before');
 
+# Test 20
 is($gene->next_feature('5utr')->_length(),109,"fill_gaps('exon','5utr','external')");
+$gff->validate_all('trna');
+$gene = $gff->by_id('gene1317');
+print $gene->as_string();
+while (my $trna = $gene->next_feature('trna')){
+	print $trna->as_string();
 
+}
 
 
 
